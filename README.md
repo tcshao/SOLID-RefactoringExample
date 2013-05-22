aed3350ff9145febcee92be565c16a4df2f62bdb
tcshao <tony.m.castellano@gmail.com>
2013-05-22 10:51:33 -0700

    initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+SOLID-RefactoringExample
+========================
+
+Related project to my blog post series which can be found here : [Part 1](http://autoincomplete.com/2013/05/23/SOLID-Intro-1of6/)
---
+++ src/Initial/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Mail;
+using System.Net.Http;
+
+namespace Initial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var beginDate = DateTime.Now.Subtract(new TimeSpan(7, 0, 0, 0));
+            decimal total = 0;
+
+            // get a total of the prices from the last 7 days
+            while (beginDate < DateTime.Now)
+            {
+                var response = new HttpClient().GetAsync("http://www.null.com/" + beginDate.ToShortDateString()).Result;
+
+                int price = int.Parse(response.Content.ReadAsStringAsync().Result);
+
+                total += price;
+                beginDate = beginDate.Add(new TimeSpan(1, 0, 0, 0));
+            }
+
+            // calculate the average from the total
+            var average = total / 7;
+
+
+            var emailMessage = new MailMessage("from@null.com", "to@null.com")
+            {
+                Body = "The forcasted price is " + average.ToString()
+            };
+
+            new SmtpClient("smtp.null.com").Send(emailMessage);
+
+        }
+    }
+}
